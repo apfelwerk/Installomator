e60a7c5a09e9ac1aab46177af0662a1459020247
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2023-09-18 08:18:17 +0200

    Create vectorworks2022sp2.sh
    Need to test if everything is working probably

---
+++ fragments/labels/vectorworks2022sp2.sh
@@ -0,0 +1,8 @@
+vectorworks2022sp2)
+    name="Vectorworks 2022 SP2"
+    appName="Vectorworks 2022 installieren.app"
+    type="dmg"
+    packageID="net.vectorworks.2022.vectorworksinstaller"
+    downloadURL="https://server3-d.vectorworks-online.de/cw/vw2022/mac/Vectorworks%202022%20SP2.dmg"
+    expectedTeamID="LFNG3Q6WX2"
+    ;;
