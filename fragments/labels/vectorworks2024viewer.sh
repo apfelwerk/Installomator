a46e4a9f8136d5cc53feab5b86642673785ee3c8
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2024-01-24 10:22:29 +0100

    Create vectorworks2024viewer.sh
    - Added Label
    - Open for testing

---
+++ fragments/labels/vectorworks2024viewer.sh
@@ -0,0 +1,7 @@
+vectorworks2024viewer)
+    name="Vectorworks Viewer 2024 installieren"
+    type="dmg"
+    downloadURL="https://download.vectorworks.de/vectorworksviewer/2024/mac"
+    appNewVersion=""
+    expectedTeamID="LFNG3Q6WX2"
+    ;;
