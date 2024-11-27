6d85292ee59e95519d0bdc4b389f6e664ad6253f
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2024-11-27 13:47:57 +0100

    Release
    10.7.6

--- Installomator.sh
+++ Installomator.sh
@@ -337,7 +337,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
         rosetta2=no
     fi
 fi
-VERSION="10.7.5"
+VERSION="10.7.6"
 VERSIONDATE="2024-11-27"
 
 # MARK: Functions
@@ -3033,20 +3033,19 @@ citrixworkspace)
     type="pkgInDmg"
     parseURL() {
         urlToParse='https://www.citrix.com/downloads/workspace-app/mac/workspace-app-for-mac-latest.html#ctx-dl-eula-external'
-        htmlDocument=$(curl -s -L $urlToParse)
+        htmlDocument=$(curl -s -L --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" $urlToParse)
         xmllint --html --xpath "string(//a[contains(@rel, 'downloads.citrix.com')]/@rel)" 2> /dev/null <(print $htmlDocument)
     }
     downloadURL="https:$(parseURL)"
     newVersionString() {
         urlToParse='https://www.citrix.com/downloads/workspace-app/mac/workspace-app-for-mac-latest.html'
-        htmlDocument=$(curl -fs $urlToParse)
+        htmlDocument=$(curl -fs --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" $urlToParse)
         xmllint --html --xpath 'string(//p[contains(., "Version")])' 2> /dev/null <(print $htmlDocument)
     }
     appNewVersion=$(newVersionString | cut -d ' ' -f2 )
     versionKey="CitrixVersionString"
     expectedTeamID="S272Y5R93J"
-    ;;
-cleartouchcollage)
+    ;;cleartouchcollage)
     name="Collage"
     type="pkgInZip"
     packageID="com.cvte.cleartouch.mac"
