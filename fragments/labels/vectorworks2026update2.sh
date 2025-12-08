e0e2b54e95e8712669164248e41712253cf1efbf
crissylines <c.dilman@apfelwerk.de>
2025-12-08 12:31:05 +0100

    Fix download URL for Vectorworks 2026 Update 2
    Corrected a typo in the downloadURL for Vectorworks 2026 Update 2 by removing the extraneous '1' at the end of the URL.

--- fragments/labels/vectorworks2026update2.sh
+++ fragments/labels/vectorworks2026update2.sh
@@ -3,6 +3,6 @@ vectorworks2026update2)
     appName="Vectorworks 2026 Installationsmanager.app"
     type="dmg"
     packageID="net.vectorworks.2026.vectorworksinstaller"
-    downloadURL="https://server1-d.vectorworks-online.de/cw/vw2026/mac/Vectorworks%202026%20Update%202.dmg1
+    downloadURL="https://server1-d.vectorworks-online.de/cw/vw2026/mac/Vectorworks%202026%20Update%202.dmg
     expectedTeamID="LFNG3Q6WX2"
     ;;
