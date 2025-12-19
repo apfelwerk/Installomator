7bfcb48107ef39e6250ecfd2d5a9e036130bbf7f
iliger <m.switon@apfelwerk.de>
2025-12-19 09:57:52 +0100

    Add ScanSnap Home label fragment script
    Introduces scansnaphome.sh to automate retrieval of the latest ScanSnap Home Offline Installer version and download URL for use in labeling workflows.

---
+++ fragments/labels/scansnaphome.sh
@@ -0,0 +1,9 @@
+scansnaphome)
+    name="SSHDownloadInstaller"
+    type="dmg"
+    appNewVersion=$(curl "https://www.pfu.ricoh.com/global/scanners/scansnap/dl/setup/m-sshoffline-2_22_0.html?MODEL=100201" | grep "ScanSnap Home Offline Installer" | sed -n 's/.*<title>\(.*\)<\/title>.*/\1/p' | sed -n 's/.*Offline Installer \(.*\) Setup Program.*/\1/p')
+    downloadURLVar1=$( echo "$appNewVersion" | sed 's/\.//g')
+    downloadURLVar2=$( echo "$appNewVersion" | sed 's/\./_/g')
+    downloadURL="https://origin.pfultd.com/downloads/ss/sshinst/m-$downloadURLVar1/MacSSHOfflineInstaller_$downloadURLVar2.dmg"
+    expectedTeamID="XW4U7W2E9L"
+    ;;
