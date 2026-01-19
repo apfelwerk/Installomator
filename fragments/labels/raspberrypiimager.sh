4c80e58bd44ada0f14aa0378a202944d06e6649b
crissylines <c.dilman@apfelwerk.de>
2026-01-19 15:05:39 +0100

    Add Raspberry Pi Imager label and update version
    Introduced support for Raspberry Pi Imager by adding its label and configuration. Updated Installomator version to 10.9.19 with corresponding date changes.

---
+++ fragments/labels/raspberrypiimager.sh
@@ -0,0 +1,6 @@
+raspberrypiimager)
+    name="Raspberry Pi Imager"
+    type="dmg"
+    downloadURL="https://downloads.raspberrypi.com/imager/imager_latest.dmg"
+    expectedTeamID="8RDZTRXE62"
+    ;;
